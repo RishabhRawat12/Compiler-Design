d9cb5ed813ef831100c66b8f0cf2ab5e1e4eb2eb
shauryat6324534 <142192285+shauryat6324534@users.noreply.github.com>
2026-03-23 22:34:56 +0530

    Add files via upload

--- src/syntax/parser.cpp
+++ src/syntax/parser.cpp
@@ -1,1 +1,195 @@
+#include "parser.h"
 
+// Constructor Pointer aur tokens initialize karne ke liye
+Parser::Parser(vector<Token> tokens,SymbolTable& st) :symTable(st)
+{
+    this->tokens = tokens;
+    this->current = 0;
+
+}
+
+// Main function Pura program yahan se parse hota hai
+vector<Stmt *> Parser::parse()
+{
+    vector<Stmt *> statements;
+    while (!isAtEnd())
+    {
+        // Faltu semicolons ya empty lines skip karne ke liye
+        if (match(SEMICOLON))
+            continue;
+
+        statements.push_back(declaration());
+    }
+    return statements;
+}
+
+//Declaration Logic: int x = 10; | string s = "something";
+Stmt *Parser::declaration()
+{
+    if (match(KEYWORD_INT) || match(KEYWORD_CHAR) || match(KEYWORD_STRING))
+    {
+        Token typeToken=previous();
+        Token name = consume(IDENTIFIER, "Give name to the variable");
+
+        if(symTable.lookup(name.lexeme)){
+            error(name,"Variable "+name.lexeme+" is already declared\n");
+        }
+
+        
+        //inserting in the symbol table
+        symTable.insert(name.lexeme,typeToken.getTypeName(), name.line);
+    
+        consume(ASSIGN, "Assignment operator = is missing");
+
+        Expr *val = expression();
+        consume(SEMICOLON, "add semicolon at the end of line");
+
+        return new DeclarationStmt(name.lexeme, val);
+    }
+
+    return statement();
+}
+
+//Statement Routing
+Stmt *Parser::statement()
+{
+    return assignment();
+}
+
+// Assignment Logic, x = 20;
+Stmt *Parser::assignment()
+{
+    Token name = consume(IDENTIFIER, "Expect identifier for assignment.");
+
+    if(!symTable.lookup(name.lexeme)){
+            error(name,"Variable "+name.lexeme+" not declared\n");
+        }
+
+    consume(ASSIGN, "Expect = after variable name.");
+
+    Expr *val = expression();
+    consume(SEMICOLON, "Expect ; after assignment value.");
+
+    return new AssignmentStmt(name.lexeme, val);
+}
+
+
+//Sabse low priority (+, -)
+Expr *Parser::expression()
+{
+    return term();
+}
+
+//Term handles the addition and subtraction
+Expr *Parser::term()
+{
+    Expr *expr = factor();
+
+    while (match(PLUS) || match(MINUS))
+    {
+        string op = previous().lexeme;
+        Expr *right = factor();
+        expr = new BinaryExpr(expr, op, right);
+    }
+    return expr;
+}
+
+//Factor handling  multiplication and division
+Expr *Parser::factor()
+{
+    Expr *expr = primary();
+
+    while (match(MULTIPLY) || match(DIVIDE))
+    {
+        string op = previous().lexeme;
+        Expr *right = primary();
+        expr = new BinaryExpr(expr, op, right);
+    }
+    return expr;
+}
+
+//Primary (Numbers, Strings, or Variables)
+
+Expr* Parser::primary() {
+    if (match(NUMBER)) {
+        return new NumberExpr(stoi(previous().lexeme));
+    }
+    
+    if (match(STRING_LITERAL)) {
+        return new StringExpr(previous().lexeme);
+    }
+    
+    if (match(CHAR_LITERAL)) {
+        return new CharExpr(previous().lexeme);
+    }
+    
+    if (match(IDENTIFIER)) {
+        Token var=previous();
+
+        //if variable is not already decalred
+        if(!symTable.lookup(var.lexeme)){
+            error(var, "Variable '" + var.lexeme + "' used without declaration!");
+        }
+        return new VariableExpr(var.lexeme);
+    }
+    
+    error(peek(), "Invalid expression syntax");
+    return nullptr;
+}
+
+// Helpers for Pointer Navigation
+
+Token Parser::advance()
+{
+    if (!isAtEnd())
+        current++;
+    return previous();
+}
+
+bool Parser::isAtEnd()
+{
+    return peek().type == END_OF_FILE;
+}
+
+Token Parser::peek()
+{
+    return tokens[current];
+}
+
+Token Parser::previous()
+{
+    return tokens[current - 1];
+}
+
+bool Parser::check(TokenType type)
+{
+    if (isAtEnd())
+        return false;
+    return peek().type == type;
+}
+
+bool Parser::match(TokenType type)
+{
+    if (check(type))
+    {
+        advance();
+        return true;
+    }
+    return false;
+}
+
+Token Parser::consume(TokenType type, string msg)
+{
+    if (check(type))
+        return advance();
+
+    error(peek(), msg);
+    return Token();
+}
+
+// Error Handling
+void Parser::error(Token token, string msg)
+{
+    cerr << "[Parser Error] Line " << token.line<< " at '" << token.lexeme << "': " << msg << endl;
+    exit(1); // Seedha terminate, no time for recovery
+}
--- src/syntax/parser.h
+++ src/syntax/parser.h
@@ -1,1 +1,45 @@
+#ifndef PARSER_H
+#define PARSER_H
 
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include "../lexical/token.h"
+#include "../AST/ast.h"
+#include "../AST/stmt.h"
+#include "../AST/expr.h"
+#include "../../symbol_table.h"
+
+class Parser {
+private:
+    vector<Token> tokens;
+    SymbolTable& symTable;
+    int current = 0;
+
+    // Logic helpers
+    Token advance();
+    bool isAtEnd();
+    Token peek();
+    Token previous();
+    bool check(TokenType type);
+    bool match(TokenType type);
+    Token consume(TokenType type, string msg);
+    void error(Token token, string msg);
+
+    // Grammar functions - Support for more types
+    Stmt* declaration();
+    Stmt* statement();
+    Stmt* assignment();
+    
+    // Expression levels for math logic
+    Expr* expression();
+    Expr* term();
+    Expr* factor();
+    Expr* primary();
+
+public:
+    Parser(vector<Token> tokens,SymbolTable& st);
+    vector<Stmt*> parse();
+};
+
+#endif
