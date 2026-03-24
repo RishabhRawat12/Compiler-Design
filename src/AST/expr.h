e6c4f76f653e0416397f4dcaf11f29e8b06cd327
Pratyush Bisht <157100295+Pratyush315@users.noreply.github.com>
2026-03-24 07:52:51 +0530

    Add expression classes for number, variable, string, and char

--- src/AST/expr.h
+++ src/AST/expr.h
@@ -1,1 +1,81 @@
+#ifndef EXPR_H
+#define EXPR_H
 
+#include "ast.h"
+
+//adding number 
+class NumberExpr : public Expr {
+public:
+    int value;
+    NumberExpr(int val) : value(val) {}
+    void print(int indent) override {
+        for(int i=0; i<indent; i++) cout << "  ";
+        cout << "NumberExpr(" << value << ")" << endl;
+    }
+};
+
+
+//adding variables
+class VariableExpr : public Expr {
+public:
+    string name;
+    VariableExpr(string n) : name(n) {}
+    void print(int indent) override {
+        for(int i=0; i<indent; i++) cout << "  ";
+        cout << "VariableExpr(" << name << ")" << endl;
+    }
+};
+
+//adding string literals
+class StringExpr : public Expr {
+public:
+    string value;
+    StringExpr(string v) : value(v) {}
+    void print(int indent) override {
+        for(int i=0; i<indent; i++) cout << "  ";
+        cout << "StringExpr(" << value << ")" << endl;
+    }
+};
+
+//Added Char Literals Node
+class CharExpr : public Expr {
+public:
+    string value;
+    CharExpr(string v) : value(v) {}
+    void print(int indent) override {
+        for(int i=0; i<indent; i++) cout << "  ";
+        cout << "CharExpr(" << value << ")" << endl;
+    }
+};
+
+
+//node  for binary operations (+, -, *, /) ke liye
+class BinaryExpr : public Expr {
+public:
+    Expr* left;
+    string op;
+    Expr* right;
+
+    BinaryExpr(Expr* l, string o, Expr* r) : left(l), op(o), right(r) {}
+
+    // Destructor 
+    ~BinaryExpr() {
+        delete left;
+        delete right;
+    }
+
+    void print(int indent) override {
+        for(int i=0; i<indent; i++) cout << "  ";
+        cout << "BinaryExpr(" << op << ")" << endl;
+        
+        for(int i=0; i<indent+1; i++) cout << "  ";
+        cout << "left:" << endl;
+        if(left) left->print(indent + 2);
+        
+        for(int i=0; i<indent+1; i++) cout << "  ";
+        cout << "right:" << endl;
+        if(right) right->print(indent + 2);
+    }
+};
+
+#endif
