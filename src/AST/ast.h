bd27be2d569153e0d0e95430d567db318f8fb706
Pratyush Bisht <157100295+Pratyush315@users.noreply.github.com>
2026-03-24 07:46:44 +0530

    Add Expr and Stmt base classes in ast.h
    Define base classes for expressions and statements in the AST.

--- src/AST/ast.h
+++ src/AST/ast.h
@@ -1,1 +1,25 @@
+#ifndef AST_BASE_H
+#define AST_BASE_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Base class for all expressions (like 5, x, etc.)
+class Expr {
+public:
+    virtual ~Expr() {}
+    // pure virtual function - printer isko use karega
+    virtual void print(int indent) = 0; 
+};
+
+// Base class for all statements (like int x = 5;)
+class Stmt {
+public:
+    virtual ~Stmt() {}
+    virtual void print(int indent) = 0;
+};
+
+#endif
